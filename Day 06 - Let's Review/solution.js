419e8b64bb0d984c2f4a8008e8afce01d12e811f
kingdragon100 <kabirdeula167@gmail.com>
2022-01-23 09:58:45 +0545

    added Day 06

---
+++ Day 06 - Let's Review/solution.js
@@ -0,0 +1,31 @@
+function processData(input) {
+    //Enter your code here
+    input = input.split('\n')
+
+    for(let i=1;i<input.length;i++){
+        var splitWord = input[i].split('');
+
+        var even = '';
+        var odd = '';
+
+        for(x=0;x<splitWord.length;x++){
+            if(x%2==0){
+                even = even + splitWord[x];
+            }else{
+                odd = odd +splitWord[x];
+            }
+        }
+        console.log(even + ' '+ odd);
+    }
+} 
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
