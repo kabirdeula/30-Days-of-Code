1814757d5d96b6253edd4c518d65608e9994c733
kingdragon100 <kabirdeula167@gmail.com>
2022-01-28 10:37:55 +0545

    added Day 11

---
+++ Day 11 - 2D Array/solution.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int main(){
+    int arr[6][6];
+    for(int i=0;i<6;i++){
+        for(int j=0;j<6;j++){
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    int max_sum = 0;
+    int temp_sum;
+    for(int i = 0; i < 4; i++) {
+        for(int j = 0; j < 4; j++) {
+            temp_sum = 0;
+            // top row
+            temp_sum += arr[i][j];
+            temp_sum += arr[i][j+1];
+            temp_sum += arr[i][j+2];
+            //middle 
+            temp_sum += arr[i+1][j+1];
+            //bottom row
+            temp_sum += arr[i+2][j];
+            temp_sum += arr[i+2][j+1];
+            temp_sum += arr[i+2][j+2];
+            
+            //if first hourglass, set as max
+            if(temp_sum > max_sum || i == 0 && j == 0)
+                max_sum = temp_sum;
+        }
+    }
+    printf("%d\n", max_sum);
+    return 0;
+}
