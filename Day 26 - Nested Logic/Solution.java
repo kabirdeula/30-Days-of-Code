ea77a6e2737fc32fb5d97a0b4dd22896f757c421
kingdragon100 <kabirdeula167@gmail.com>
2022-02-12 16:01:26 +0545

    added Day 26

--- Day 26 - Nested Logic/Solution.java
+++ Day 26 - Nested Logic/Solution.java
@@ -1,1 +1,24 @@
-// Day 26
+import java.util.Scanner;
+
+public class Solution{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        int da = input.nextInt();
+        int ma = input.nextInt();
+        int ya = input.nextInt();
+
+        int de = input.nextInt(); 
+        int me = input.nextInt();
+        int ye = input.nextInt();
+
+        int fine = 0;
+
+        if(ya > ye) fine = 10000;
+        else if (ya == ye){
+            if (ma > me) fine = (ma - me) * 500;
+            else if (ma == me && da > de) fine = (da - de) * 15;
+        }
+        System.out.println(fine);
+    }
+}
